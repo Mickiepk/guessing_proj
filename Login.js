d57a16f985151c319445218e7f73f1dac4db1a37
Mickiepk <115391401+Mickiepk@users.noreply.github.com>
2024-07-18 12:35:19 +0700

    Update Login2.js

--- Login.js
+++ Login.js
@@ -1,24 +1,33 @@
 import React, { useState, useContext } from 'react';
 import { AuthContext } from './AuthContext';
 
+
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const { setToken } = useContext(AuthContext);
 
     const handleLogin = async () => {
-        const response = await fetch('/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username, password })
-        });
-        const data = await response.json();
-        if (data.token) {
-            setToken(data.token);
-            setMassage("Login Success")
-        }
-        else {
-            setMassage("Login Failed")
+        try {
+            const response = await fetch('http://localhost:8080/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ username, password })
+            });
+    
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+    
+            const data = await response.json();
+            if (data.token) {
+                setToken(data.token);
+                window.location.href = '/guess';
+            } else {
+                console.error('Login failed: ', data);
+            }
+        } catch (error) {
+            console.error('Failed to fetch: ', error);
         }
     };
 
@@ -27,7 +36,7 @@ const Login = () => {
             <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
             <button onClick={handleLogin}>Login</button>
-            {message && <p>{message}</p>}
+            
         </div>
     );
 }
