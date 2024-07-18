ac3a68dacdd09f5405361b953bad49a43d9fa813
Mickiepk <115391401+Mickiepk@users.noreply.github.com>
2024-07-18 10:33:18 +0700

    Update Login.js

--- Login.js
+++ Login.js
@@ -15,6 +15,10 @@ const Login = () => {
         const data = await response.json();
         if (data.token) {
             setToken(data.token);
+            setMassage("Login Success")
+        }
+        else {
+            setMassage("Login Failed")
         }
     };
 
@@ -23,8 +27,9 @@ const Login = () => {
             <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
             <button onClick={handleLogin}>Login</button>
+            {message && <p>{message}</p>}
         </div>
     );
-};
+}
 
 export default Login;
