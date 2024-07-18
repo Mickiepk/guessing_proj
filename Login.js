e331bff6b176627e6ae7ff9db582c0f5af62014a
Mickiepk <115391401+Mickiepk@users.noreply.github.com>
2024-07-18 23:08:53 +0700

    Tested_versionjs

--- Login.js
+++ Login.js
@@ -2,45 +2,56 @@
 import React, { useState, useContext } from 'react';
 import { AuthContext } from './AuthContext';
 import axios from 'axios';
-
+import './Login.css';
+import {
+    useNavigate
+} from 'react-router-dom';
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const { setToken } = useContext(AuthContext);
+    const navigate = useNavigate();
 
-    const handleLogin = async () => {
+    
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        
         try {
             console.log('Sending login request to backend:', { username, password });
             const response = await axios.post('http://localhost:8080/login', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ username, password })
+                username: username,
+                password: password
             });
-    
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-    
-            const data = await response.json();
-            console.log('Received response from backend:', data);
-            if (data.token) {
-                setToken(data.token);
-                window.location.href = '/guess';
-            } else {
-                console.error('Login failed: ', data);
-            }
+
+            console.log('Login successful:', response.data);
+            setToken(response.data.token);
+            navigate('/guess');
         } catch (error) {
-            console.error('Failed to fetch: ', error);
+            console.error('Error logging in:', error.response ? error.response.data : error.message);
         }
     };
 
     return (
-        <div>
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
-            <button onClick={handleLogin}>Login</button>
-            
+        <form onSubmit={handleLogin}>
+        <div className="login-container">
+            <input 
+                type="text" 
+                value={username} 
+                onChange={(e) => setUsername(e.target.value)} 
+                placeholder="Username" 
+                className="login-input"
+            />
+            <input 
+                type="password" 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)} 
+                placeholder="Password" 
+                className="login-input"
+            />
+            <button type='submit' className="login-button">Login</button>
         </div>
+        </form>
     );
 }
 
