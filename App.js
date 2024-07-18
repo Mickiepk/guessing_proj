5f3d7b35b2b79aee3e973a579c60b17641b94714
Mickiepk <115391401+Mickiepk@users.noreply.github.com>
2024-07-18 13:40:37 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,25 +1,21 @@
-// App.js
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './AuthContext';
 import Login from './Login';
 import Guess from './Guess';
-import {
-  Routes,
-  Route,
-  Link,
-  useNavigate,
-  useLocation,
-  Navigate,
-  Outlet,
-} from 'react-router-dom';
 
 const App = () => {
   return (
     <AuthProvider>
-      <div className="App">
-        <Login />
-        <Guess />
-      </div>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/guess" element={<Guess />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+        </div>
+      </Router>
     </AuthProvider>
   );
 }
